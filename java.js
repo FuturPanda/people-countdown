f5167b941d466dac14c5681f050393a3650bfcfb
Hosaro <sty.hoareau@gmail.com>
2022-12-06 21:46:27 +0100

    Simplify the result of the game
    Add an array for the different win combinations, and look in it to
    know who wins.
    Less precision for the user to know what the computer did.

--- java.js
+++ java.js
@@ -1,43 +1,40 @@
-
+// Set the different moves, as well as the set of moves where the player win
 let gameMoves = ['paper', 'rock', 'scissors']; 
+let playerWin = [["rock","scissors"],['paper', 'rock'], ["scissors",'paper']]
 
+// Set a random choice for the computer
 function getComputerChoice(randomItem) {
   return randomItem[Math.floor(Math.random()*randomItem.length)]
 }
-let playerSelection =  (prompt("what's your move ?")).toLowerCase();
 let computerSelection = getComputerChoice(gameMoves)
 
-console.log(computerSelection)
+//Get the user's choice, case insensitive.
+let playerSelection =  (prompt("what's your move ?")).toLowerCase();
 
-function playGround() {
-  if (computerSelection === playerSelection )
-  console.log ("Draw ! try again")
-  else if (playerSelection === "Paper") {
-    if (computerSelection === 'Rock')
-    console.log ("You Win ! Paper beats Rock")
-    else if (computerSelection === 'Scissors')
-    console.log ("You Loose ! Paper get beat by Scissors")
-  }
-  else if (playerSelection === "Rock") {
-    if (computerSelection === 'Paper')
-    console.log ("You Loose ! Paper beats Rock")
-    else if (computerSelection === 'Scissors')
-    console.log ("You Win ! Rock beats Scissors everytime")
-  }
-  else if (playerSelection === "Scissors") {
-    if (computerSelection === 'Rock')
-    console.log ("You Loose ! Rock still Beat Scissors")
-    else if (computerSelection === 'Paper')
-    console.log ("You Win ! Paper gets beat by Scissors")
-  }
-  else 
-  console.log ("Interdit de tricher ! ")
+// Compare the two moves to know if there's a win combination for the player
+function matchResult(movePlayer, moveComputer) {
+  let fightMoves = [movePlayer, moveComputer]
+  let whoWins = playerWin.includes(fightMoves) 
+  if (whoWins == true)
+  return true 
+  else
+  return false
 }
 
-playGround();
-/*
-1. Get the Computer Choice
-2. Store the player fix choice for the beggining. 
-3. 
+// Create a round of the game.
+function playGround() {
+  matchResult(playerSelection, computerSelection);
+  let resultPlay = matchResult(playerSelection, computerSelection);
+  if (computerSelection === playerSelection )
+  return ("Draw ! try again")
+  else if (resultPlay == true)
+  return ("You Win!")
+  else
+  return ("You Lose ! ")
+}
 
-*/
+// Start the game and write the result. 
+playGround();
+let resultRound = playGround();
+console.log(resultRound);
+ 
