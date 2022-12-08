e1bd152134c4b15ae0cc0a28ded8bebc3db637b8
Hosaro <sty.hoareau@gmail.com>
2022-12-08 16:14:00 +0100

    Debug winner selection

--- java.js
+++ java.js
@@ -2,39 +2,62 @@
 let gameMoves = ['paper', 'rock', 'scissors']; 
 let playerWin = [["rock","scissors"],['paper', 'rock'], ["scissors",'paper']]
 
-// Set a random choice for the computer
-function getComputerChoice(randomItem) {
-  return randomItem[Math.floor(Math.random()*randomItem.length)]
-}
-let computerSelection = getComputerChoice(gameMoves)
+// Create Leaderboard
+let scoreGame = [0, 0];
 
-//Get the user's choice, case insensitive.
-let playerSelection =  (prompt("what's your move ?")).toLowerCase();
-
-// Compare the two moves to know if there's a win combination for the player
-function matchResult(movePlayer, moveComputer) {
-  let fightMoves = [movePlayer, moveComputer]
-  let whoWins = playerWin.includes(fightMoves) 
-  if (whoWins == true)
-  return true 
-  else
-  return false
-}
 
 // Create a round of the game.
 function playGround() {
-  matchResult(playerSelection, computerSelection);
-  let resultPlay = matchResult(playerSelection, computerSelection);
+
+  // Set a random choice for the computer
+  function getComputerChoice(randomItem) {
+    return randomItem[Math.floor(Math.random()*randomItem.length)]
+  }
+  let computerSelection = "paper"
+  
+  //getComputerChoice(gameMoves)
+
+  //Get the user's choice, case insensitive.
+  let playerSelection = (prompt("what's your move ?")).toLowerCase();
+
+  //Determine the winner
+  let matchResult = playerSelection + computerSelection
   if (computerSelection === playerSelection )
-  return ("Draw ! try again")
-  else if (resultPlay == true)
-  return ("You Win!")
+   { prompt ("Draw ! try again")}
+  else if (matchResult == ["rock","scissors"] || ['paper', 'rock'] || ["scissors",'paper'])
+    {scoreGame[0] += 1
+    prompt ("You Win!" + "Player : " + scoreGame[0] + " Ordi : " + scoreGame[1])}
   else
-  return ("You Lose ! ")
+    {scoreGame[1] += 1
+    prompt ("You Lose ! " + "Player : " + scoreGame[0] + " Ordi : " + scoreGame[1])}
 }
 
+
+function gameStart() {
+  if (scoreGame.length == 0) {
+    prompt("Welcome to the playground where you can play ShiFuMi till one's dead ! ")
+  }
+  else if (scoreGame.includes(5) == false) {
+    prompt("La partie continue")
+  }
+  while (scoreGame.includes(4) == false){
+    playGround()
+  }
+  if (scoreGame[0] == 5){
+    prompt("Bravo soldat, t'a gagné")
+  }
+  else if(scoreGame[1] == 5){
+    prompt("Dommage... Tu m'a déçu")
+  }
+}
+
+
+
+
 // Start the game and write the result. 
-playGround();
+gameStart()
 let resultRound = playGround();
 console.log(resultRound);
+console.log(computerSelection);
+console.log(playerSelection);
  
